c237439ae4fab3fbece1bc427db7048a820d2c1b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-03-30 12:48:27 +0900

    import

---
+++ src/sample/PollerService.java
@@ -0,0 +1,83 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.scene.control.Alert;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PollerService extends ScheduledService<String> {
+    private final Connection connection;
+    private final ObservableList<SampleData> sampleDataObservableList;
+    private int samples;
+    private final Map<String, Integer> data;
+
+    public PollerService(Connection connection, ObservableList<SampleData> sampleDataObservableList) {
+        this.connection = connection;
+        this.sampleDataObservableList = sampleDataObservableList;
+        this.samples = 0;
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    protected Task<String> createTask() {
+        return new Task<String>() {
+            private QueryNormalizer queryNormalizer = new QueryNormalizer();
+
+            protected String call() throws IOException {
+                // get samples
+                try {
+                    samples++;
+                    PreparedStatement preparedStatement = connection.prepareStatement("SHOW FULL PROCESSLIST");
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    resultSet.next();
+                    while (resultSet.next()) {
+                        String info = resultSet.getString("Info");
+                        if ("SHOW FULL PROCESSLIST".equals(info)) {
+                            continue;
+                        }
+                        if (info != null) {
+                            String query = queryNormalizer.normalize(info);
+                            if (data.containsKey(query)) {
+                                data.put(query, data.get(query) + 1);
+                            } else {
+                                data.put(query, 1);
+                            }
+                        }
+                    }
+                } catch (SQLException e) {
+                    Platform.runLater(() -> {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Exception");
+                        alert.setHeaderText("Gah!!!! Exception was occurred.");
+                        alert.setContentText(e.getMessage());
+
+                        alert.showAndWait();
+                    });
+                }
+
+                // update table view
+                Platform.runLater(() -> {
+                    sampleDataObservableList.clear();
+                    sampleDataObservableList.addAll(
+                            data.entrySet()
+                                    .stream()
+                                    .sorted((a, b) -> a.getValue() - b.getValue())
+                                    .map(it -> new SampleData(it.getValue() / (samples * 100.0), it.getKey()))
+                                    .toArray(SampleData[]::new)
+                    );
+                });
+
+                return "";
+            }
+        };
+    }
+}
